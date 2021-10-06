390e394de6d5f1fe8db32a4e1fd26124a3e2d65e
Ian Christiansen <ianc.songdog@gmail.com>
2021-10-06 13:21:24 -0700

    fetched dom elements and declared global states

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 import { didUserWin } from "./utils";
+// DOM ELEMENTS
+const playButton = document.getElementById('play');
+const spanEscaped = document.getElementById('escaped');
+const spanSacrificed = document.getElementById('sacrificed');
+const spanStandoff = document.getElementById('standoff');
+const error = document.getElementById('error');
 
+// GLOBAL STATE
+let escaped = 0;
+let sacrificed = 0;
+let standoff = 0;
+
+//EVENT LISTENERS
