d27a742ee075ef15cf37af4de9fadfa0aa01a7d6
James Demiraiakian <james.armen@gmail.com>
2021-10-08 15:56:29 -0700

    fixed images, working on reset

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 import { didUserWin, getRandomMove, outcome } from './utils.js';
 
 const playButton = document.getElementById('play');
+const spanEscaped = document.getElementById('escaped');
+const spanSacrificed = document.getElementById('sacrificed');
+const spanStandoff = document.getElementById('standoff');
 const error = document.getElementById('error');
 const result = document.getElementById('result');
 const reset = document.getElementById('reset');
-// const imageBlind = document.getElementById('blind');
-// const imageDead = document.getElementById('dead');
 const meg = document.getElementById('meg');
 const flashlight = document.getElementById('flashlight');
 const pig = document.getElementById('pig');
+
+let escaped = 0;
+let sacrificed = 0;
+let standoff = 0;
 
 meg.addEventListener('click', ()=>{
     outcome('meg');
